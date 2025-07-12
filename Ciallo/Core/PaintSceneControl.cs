f5349ec758e7558b54dc6212b2a64696e78e02e5
ShenCiao <3294833183@qq.com>
2025-07-12 19:55:06 +0800

    Rename and explore subviewport

--- Ciallo/Core/PaintSceneControl.cs
+++ Ciallo/Core/PaintSceneControl.cs
@@ -11,6 +11,9 @@ public partial class PaintSceneControl : Control
 
     public void OnResize()
     {
-        _SubViewport.Size = new(Mathf.RoundToInt(Size.X), Mathf.RoundToInt(Size.Y));
+        var size = this.Size;
+        // SubViewportContainer uses the combined size of the SubViewports as minimum size,
+        // https://docs.godotengine.org/en/stable/classes/class_subviewportcontainer.html
+        _SubViewport.Size = new(Mathf.RoundToInt(size.X), Mathf.RoundToInt(size.Y));
     }
 }
