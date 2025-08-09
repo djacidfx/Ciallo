352f44cedef145aad240a6089b42bb93c9cb9d2c
ShenCiao <3294833183@qq.com>
2025-08-10 01:58:13 +0800

    Remove MemoryPack package

--- Ciallo/Misc/Autoload.cs
+++ Ciallo/Misc/Autoload.cs
@@ -1,5 +1,4 @@
 ﻿using Godot;
-using MemoryPack;
 using ObservableCollections;
 using R3;
 
@@ -9,8 +8,6 @@ public partial class Autoload : Node
 {
     public override void _EnterTree()
     {
-        MemoryPackFormatterProvider.RegisterGenericType(typeof(ReactiveProperty<>), typeof(ReactivePropertyFormatter<>));
-        MemoryPackFormatterProvider.RegisterGenericType(typeof(ObservableList<>), typeof(ObservableListFormatter<>));
         
         // Handle quit manually (to save unsaved file)
         // GetTree().AutoAcceptQuit = false;
