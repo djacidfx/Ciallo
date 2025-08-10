f30d122a19fa68c615f37dc1a90861bf460b1eaa
ShenCiao <3294833183@qq.com>
2025-08-10 11:07:43 +0800

    Renaming and reicon

--- Ciallo/Main.cs
+++ Ciallo/Main.cs
@@ -3,5 +3,8 @@ using System;
 
 public partial class Main : Node
 {
-    
+    public override void _Ready()
+    {
+        
+    }
 }
--- Ciallo/Misc/Autoload.cs
+++ Ciallo/Misc/Autoload.cs
@@ -1,6 +1,12 @@
-﻿using Godot;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Godot;
 using ObservableCollections;
 using R3;
+using Arch.Core;
+using MessagePack;
 
 namespace Ciallo.Misc;
 
@@ -22,5 +28,22 @@ public partial class Autoload : Node
     public override void _Ready()
     {
         
+    }
+    
+    public static IEnumerable<Type> GetSerializableTypes()
+    {
+        var allTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a =>
+        {
+            try
+            {
+                return a.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(t => t != null);
+            }
+        }).Where(t => t is { IsAbstract: false });
+        
+        return allTypes.Where(t => t!.GetCustomAttributes(typeof(MessagePackObjectAttribute), false).Length > 0);
     }
 }
