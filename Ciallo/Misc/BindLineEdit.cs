571a113a9b33ec42fba72d81dddcd053e1e9acf6
ShenCiao <3294833183@qq.com>
2025-09-06 02:02:15 +0800

    Fix bug in layer name editing

--- Ciallo/Misc/BindLineEdit.cs
+++ Ciallo/Misc/BindLineEdit.cs
@@ -8,7 +8,12 @@ public static class BindLineEdit
     public static CompositeDisposable BindString(this LineEdit lineEdit, ReactiveProperty<string> property)
     {
         var subs = new CompositeDisposable();
-        property.Subscribe(value => lineEdit.Text = value).AddTo(subs);
+        property.Subscribe(value =>
+        {
+            // `lineEdit.Text = value` makes the control reset its cursor. Must manually check to get correct input behavior.
+            if(lineEdit.Text == value) return; 
+            lineEdit.Text = value;
+        }).AddTo(subs);
         lineEdit.OnTextChangedAsObservable()
             .Subscribe(value => property.Value = value).AddTo(subs);
         return subs;
