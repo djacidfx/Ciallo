0905e48baa3b88f69b1f303fc96d444c49aa8f8d
ShenCiao <3294833183@qq.com>
2025-09-10 01:05:20 +0800

    Link button to About popup

--- Ciallo/NodeControl/MenuHelp.cs
+++ Ciallo/NodeControl/MenuHelp.cs
@@ -1,9 +1,44 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ciallo.Command;
 
 namespace Ciallo.NodeControl;
 
 public partial class MenuHelp : PopupMenu
 {
+    public static readonly OrderedDictionary<string, AppAction> MenuItems = new()
+    {
+        { "User manual", null },
+        { "About Ciallo", null },
+    };
     
+    public override void _Ready()
+    {
+        foreach(var (i, item) in MenuItems.Index())
+        {
+            if(item.Key.StartsWith('-'))
+            {
+                AddSeparator();
+                continue;
+            }
+            AddItem(Tr(item.Key));
+            if (item.Value != null) SetItemShortcut(i, item.Value.Shortcut);
+        }
+        
+        IndexPressed += id => OnIndexPressed((int)id);
+    }
+
+    private void OnIndexPressed(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                GetTree().GetNodesInGroup("Dialog").OfType<AcceptDialog>().First(n => n.Name == "AboutCiallo").Popup();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(id), $"Unhandled menu item index: {id}");
+        }
+    }
 }
