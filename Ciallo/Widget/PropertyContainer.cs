2aec478354c23ca46de026b8b8bfc408319a9990
ShenCiao <3294833183@qq.com>
2025-09-14 22:06:07 +0800

    Aesthetical modification

--- Ciallo/Widget/PropertyContainer.cs
+++ Ciallo/Widget/PropertyContainer.cs
@@ -8,12 +8,17 @@ public partial class PropertyContainer : VBoxContainer
 {
     public override void _EnterTree()
     {
-        
+        AddThemeConstantOverride("v_separation", 10);
     }
 
     public void AddPropertyControl(string name, [NotNull] Control control)
     {
-        var box = new HBoxContainer();
+        var box = new HFlowContainer()
+        {
+            LastWrapAlignment = FlowContainer.LastWrapAlignmentMode.End,
+        };
+        box.AddThemeConstantOverride("h_separation", 30);
+        box.AddThemeConstantOverride("v_separation", 0);
         box.AddChild(new Label
         {
             Text = name,
@@ -21,7 +26,7 @@ public partial class PropertyContainer : VBoxContainer
             HorizontalAlignment = HorizontalAlignment.Left,
             SizeFlagsVertical = SizeFlags.Fill,
         });
-        control.SizeFlagsHorizontal = SizeFlags.ShrinkEnd | SizeFlags.Expand;
+        control.SizeFlagsHorizontal = SizeFlags.ExpandFill;
         box.AddChild(control);
         AddChild(box);
     }
