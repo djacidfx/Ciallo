20a921aa926720a155e03cd1d2ebb1797f38d585
ShenCiao <3294833183@qq.com>
2025-09-16 01:16:07 +0800

    Add airbrush support

--- Ciallo/Rendering/RenderingAutoload.cs
+++ Ciallo/Rendering/RenderingAutoload.cs
@@ -6,7 +6,7 @@ public partial class RenderingAutoload : Node
 {
     public override void _Ready()
     {
-        StrokeView.BrushMaterial.SetShaderParameter("strokeType", 1);
+        StrokeView.BrushMaterial.SetShaderParameter("strokeType", 2);
         StrokeView.BrushMaterial.SetShaderParameter("strokeColor", Colors.Black);
         StrokeView.BrushMaterial.SetShaderParameter("stampInterval", 2.0f);
         StrokeView.BrushMaterial.SetShaderParameter("radiusMode", 0);
