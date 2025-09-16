353f119e384147880a4128c53a48b6f47f41214b
ShenCiao <3294833183@qq.com>
2025-09-16 23:01:46 +0800

    Bind observable list

--- Ciallo/Misc/BindItemList.cs
+++ Ciallo/Misc/BindItemList.cs
@@ -1,31 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics.CodeAnalysis;
 using Godot;
+using ObservableCollections;
 using R3;
 
 namespace Ciallo.Misc;
 
+// Note: DeselectAll does not emit ItemSelected signal
 public static class BindItemList
 {
-    public static CompositeDisposable BindValue<T>([NotNull] this ItemList list, [NotNull] IReadOnlyList<T> items, 
+    // Fix item list
+    public static CompositeDisposable BindValue<T>(this ItemList control, [NotNull] IReadOnlyList<T> items, 
         [NotNull] ReactiveProperty<T> property, Func<T, string> toString = null)
     {
-        if (list.SelectMode != ItemList.SelectModeEnum.Single) throw new ArgumentException("Must be single", nameof(list));
-        list.Clear();
+        if (control.SelectMode != ItemList.SelectModeEnum.Single) throw new ArgumentException("List must be single selectable", nameof(control));
+        control.Clear();
         foreach (var item in items)
-            list.AddItem(toString != null ? toString(item) : item.ToString());
+            control.AddItem(toString != null ? toString(item) : item.ToString());
         
         var subs = new CompositeDisposable();
         property.Subscribe(value =>
         {
-            list.DeselectAll();
             var idx = items.IndexOf(value);
-            if (idx >= 0) list.Select(idx);
+            control.Select(idx);
         }).AddTo(subs);
 
-        list.SignalAsObservable<long>(ItemList.SignalName.ItemSelected)
-            .Subscribe(index => property.Value = items[(int)index])
+        control.SignalAsObservable<long>(ItemList.SignalName.ItemSelected)
+            .Subscribe(idx => property.Value = items[(int)idx])
+            .AddTo(subs);
+        return subs;
+    }
+    
+    // Dynamic list view created from R3.ObservableList
+    public static CompositeDisposable BindValue<T>(this ItemList control, IWritableSynchronizedView<T,string> view,
+        [NotNull] ReactiveProperty<T> property)
+    {
+        var subs = new CompositeDisposable();
+        
+        foreach (var itemString in view)
+            control.AddItem(itemString);
+        view.ObserveChanged().Subscribe(e =>
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    control.AddItem(e.NewItem.View);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    control.RemoveItem(e.OldStartingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    control.SetItemText(e.NewStartingIndex, e.NewItem.View);
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    control.MoveItem(e.OldStartingIndex, e.NewStartingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    control.Clear();
+                    break;
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }).AddTo(subs);
+        
+        property.Subscribe(value =>
+        {
+            int idx = -1;
+            for(int i = 0; i < view.Count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(view.GetAt(i).Value, value))
+                {
+                    idx = i;
+                    break;
+                }
+            }
+            control.Select(idx);
+        }).AddTo(subs);
+        
+        control.SignalAsObservable<long>(ItemList.SignalName.ItemSelected)
+            .Subscribe(idx => property.Value = view.GetAt((int)idx).Value)
             .AddTo(subs);
         return subs;
     }
