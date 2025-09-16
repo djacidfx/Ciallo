5e294e4d8e85fbbb6ed96fafa49200f6fdec3879
ShenCiao <3294833183@qq.com>
2025-09-16 10:21:24 +0800

    Set up brush manager ui

---
+++ Ciallo/NodeControl/MenuWindow.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ciallo.Command;
+
+public partial class MenuWindow : PopupMenu
+{
+    public static readonly OrderedDictionary<string, AppAction> MenuItems = new()
+    {
+        { "Brush manager", null },
+    };
+    
+    public override void _Ready()
+    {
+        foreach(var (i, item) in MenuItems.Index())
+        {
+            if(item.Key.StartsWith('-'))
+            {
+                AddSeparator();
+                continue;
+            }
+            AddItem(Tr(item.Key));
+            if (item.Value != null) SetItemShortcut(i, item.Value.Shortcut);
+        }
+        
+        IndexPressed += id => OnIndexPressed((int)id);
+    }
+    
+    private void OnIndexPressed(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                GetTree().GetNodesInGroup("Dialog").OfType<BrushManager>().First().Popup();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(id), $"Unhandled menu item index: {id}");
+        }
+    }
+}
