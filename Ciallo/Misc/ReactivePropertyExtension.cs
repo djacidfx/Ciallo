f9328dde69b1cf1c95963e01dde5f94908261f33
ShenCiao <3294833183@qq.com>
2025-09-22 13:26:15 +0800

    Reactive property view

---
+++ Ciallo/Misc/ReactivePropertyExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using R3;
+
+namespace Ciallo.Misc;
+
+public static class ReactivePropertyExtension
+{
+    public static ReactivePropertyView<TView> CreateView<T, TView>(this ReactiveProperty<T> property, Func<T, TView> transform)
+    {
+        var view =  new ReactivePropertyView<TView>();
+        property.Subscribe(value => view.Value = transform(value)).AddTo(view);
+        return view;
+    }
+
+    public static void AddTo<T>(this IDisposable disposable, ReactivePropertyView<T> view)
+    {
+        view.AddDisposable(disposable);
+    }
+}
+
+public class ReactivePropertyView<T> : ReactiveProperty<T>
+{
+    // ReSharper disable once CollectionNeverQueried.Global
+    public CompositeDisposable ExtraDisposable { get; } = new();
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        ExtraDisposable.Dispose();
+    }
+    
+    public void AddDisposable(IDisposable disposable)
+    {
+        ExtraDisposable.Add(disposable);
+    }
+}
