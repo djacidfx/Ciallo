1015c38d93d6d39502120bb57f95b88cdeca4c80
ShenCiao <3294833183@qq.com>
2025-09-28 18:43:45 +0800

    Add mipmap to airbrush

--- Ciallo/Rendering/BrushMaterial.cs
+++ Ciallo/Rendering/BrushMaterial.cs
@@ -47,8 +47,10 @@ public partial class BrushMaterial : ShaderMaterial
 
     public static Image BakeCurve(BezierCurve curve)
     {
-        var data = curve.SampleXList(Enumerable.Range(0, 127).Select(i => i / 127f).ToArray());
+        int n = 256;
+        var data = curve.SampleXList(Enumerable.Range(0, n).Select(i => (float)i / n).ToArray());
         var img = Image.CreateFromData(data.Count, 1, false, Image.Format.L8, data.Select(c => (byte)(c * 255)).ToArray());
+        img.GenerateMipmaps();
         // img.GenerateMipmaps();
         return img;
     }
