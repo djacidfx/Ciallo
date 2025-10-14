265f50e588180a82509b80e22df7e819498e8eea
ShenCiao <3294833183@qq.com>
2025-10-14 23:05:51 +0800

    Points on image edit overlay

--- Ciallo/Rendering/AutoloadRendering.cs
+++ Ciallo/Rendering/AutoloadRendering.cs
@@ -4,14 +4,32 @@ namespace Ciallo.Rendering;
 
 public partial class AutoloadRendering : Node
 {
-    public static readonly ShaderMaterial WireframeMaterial = GD.Load<ShaderMaterial>("res://Rendering/WireframeMaterial.tres");
-    public static readonly ShaderMaterial WireframeDotMaterial = GD.Load<ShaderMaterial>("res://Rendering/WireframeDotMaterial.tres");
+    public static readonly ShaderMaterial WireframeMaterial =
+        GD.Load<ShaderMaterial>("res://Rendering/WireframeMaterial.tres");
+
+    public static readonly ShaderMaterial WireframeDotMaterial =
+        GD.Load<ShaderMaterial>("res://Rendering/WireframeDotMaterial.tres");
+
     public static readonly Shader StrokeShader = GD.Load<Shader>("res://Rendering/Stroke.gdshader");
     public static readonly Mesh DummyMesh = GD.Load<Mesh>("res://Rendering/StrokeDummyMesh.tres");
-    
+    public static readonly Mesh WireframeDotMesh = GD.Load<Mesh>("res://Rendering/WireframeDotMesh.tres");
+
     public override void _Ready()
     {
         StrokeShader.TakeOverPath("");
         DummyMesh.TakeOverPath("");
+    }
+
+    public static MultiMeshInstance2D CreateDots()
+    {
+        return new()
+        {
+            Material = WireframeDotMaterial,
+            Multimesh = new MultiMesh
+            {
+                UseColors = true,
+                Mesh = WireframeDotMesh,
+            }
+        };
     }
 }
--- Ciallo/Rendering/ImageLayerOverlay.cs
+++ Ciallo/Rendering/ImageLayerOverlay.cs
@@ -6,6 +6,7 @@ public partial class ImageLayerOverlay : Node2D
 {
     public Vector2[] Corners;
     public StrokeView Wireframe;
+    public MultiMeshInstance2D Dots;
     public TouchScreenButton[] Buttons;
 
     public ImageLayerOverlay(Vector2[] corners)
@@ -20,5 +21,9 @@ public partial class ImageLayerOverlay : Node2D
         Vector2[] positions = [..Corners, Corners[0]];
         Wireframe.SetGeometry(positions, AppPreference.StrokeWireframeRadius);
         AddChild(Wireframe);
+        
+        Dots = AutoloadRendering.CreateDots();
+        Dots.SetDotGeometry(positions, AppPreference.StrokeDotRadius * 2);
+        AddChild(Dots);
     }
 }
