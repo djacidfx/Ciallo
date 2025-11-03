9c251c2b2e53d8a1680c015a2997a505e73bcb0f
ShenCiao <3294833183@qq.com>
2025-11-04 00:44:33 +0800

    QuadraticBezier math (but not use it for interpolation after testing)

---
+++ Ciallo/Geometry/QuardraticBezierExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Contracts;
+using Godot;
+
+namespace Ciallo.Geometry;
+
+public static class QuadraticBezierExtension
+{
+    /// <summary>
+    /// Identical to Vector2.BezierInterpolate but for quadratic Bezier curves.
+    /// </summary>
+    [Pure] public static Vector2 QuadraticBezierInterpolate(this Vector2 p0, Vector2 p1, Vector2 p2, float t)
+    {
+        float u = 1f - t;
+        return (u * u) * p0 + (2f * u * t) * p1 + (t * t) * p2;
+    }
+
+    [Pure] public static Vector2 QuadraticBezierDerivative(this Vector2 p0, Vector2 p1, Vector2 p2, float t)
+    {
+        float u = 1f - t;
+        return 2f * (u * (p1 - p0) + t * (p2 - p1));
+    }
+}
