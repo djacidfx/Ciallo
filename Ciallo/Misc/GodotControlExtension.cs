812cf39cbed966efec4f25ad67c727e292d672e4
ShenCiao <3294833183@qq.com>
2025-11-06 23:23:38 +0800

    Refine VisibleIf api

--- Ciallo/Misc/GodotControlExtension.cs
+++ Ciallo/Misc/GodotControlExtension.cs
@@ -7,19 +7,22 @@ namespace Ciallo.Misc;
 
 public static class GodotControlExtension
 {
-    public static void VisibleIf<T>(this Control control, ReadOnlyReactiveProperty<T> property, Predicate<T> predicate, out IDisposable sub)
+    public static TControl VisibleIf<TControl, T>(this TControl control, Observable<T> property, Predicate<T> predicate, out IDisposable sub) where TControl : Control
     {
         sub = property.Subscribe(value => control.Visible = predicate(value));
+        return control;
     }
 
-    public static void VisibleIf<T>(this Control control, ReadOnlyReactiveProperty<T> property, Predicate<T> predicate)
+    public static TControl VisibleIf<TControl, T>(this TControl control, Observable<T> property, Predicate<T> predicate) where TControl : Control
     {
         control.VisibleIf(property, predicate, out var sub);
         sub.AddTo(control);
+        return control;
     }
 
-    public static void VisibleIf<T>(this Control control, ReadOnlyReactiveProperty<T> property, T value)
+    public static TControl VisibleIf<TControl, T>(this TControl control, Observable<T> property, T value) where TControl : Control
     {
         control.VisibleIf(property, v => EqualityComparer<T>.Default.Equals(v, value));
+        return control;
     }
 }
