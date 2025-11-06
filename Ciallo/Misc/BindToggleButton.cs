d5df856a8f34eaa86ebdf9350af16b7cc4a20da0
ShenCiao <3294833183@qq.com>
2025-11-06 11:49:40 +0800

    Checkbox add bitflags support

--- Ciallo/Misc/BindToggleButton.cs
+++ Ciallo/Misc/BindToggleButton.cs
@@ -6,23 +6,56 @@ namespace Ciallo.Misc;
 
 public static class BindToggleButton
 {
-    private static CompositeDisposable BindBool(this BaseButton button, ReactiveProperty<bool> property)
+    private static void BindBool(this BaseButton button, ReactiveProperty<bool> property, out CompositeDisposable subs)
     {
         if (!button.ToggleMode) throw new ArgumentException("Button must be in toggle mode", nameof(button));
-        var subs = new CompositeDisposable();
+        subs = new CompositeDisposable();
         property.Subscribe(value => button.ButtonPressed = value).AddTo(subs);
         button.OnToggledAsObservable()
             .Subscribe(value => property.Value = value).AddTo(subs);
-        return subs;
     }
 
-    public static CompositeDisposable BindBool(this CheckBox checkBox, ReactiveProperty<bool> property)
+    public static void BindBool(this CheckBox checkBox, ReactiveProperty<bool> property, out CompositeDisposable subs)
     {
-        return BindBool((BaseButton)checkBox, property);
+        BindBool((BaseButton)checkBox, property, out subs);
     }
 
-    public static CompositeDisposable BindBool(this Button button, ReactiveProperty<bool> property)
+    public static void BindBool(this CheckBox checkBox, ReactiveProperty<bool> property)
     {
-        return BindBool((BaseButton)button, property);
+        BindBool(checkBox, property, out var sub);
+        sub.AddTo(checkBox);
+    }
+
+    /// <summary>
+    /// Bind a bitflag property to a CheckBox.
+    /// </summary>
+    /// <param name="checkBox"></param>
+    /// <param name="property"></param>
+    /// <param name="mask">The bits to toggle on and off</param>
+    /// <param name="subs"></param>
+    /// <typeparam name="T">Enum with FlagsAttribute</typeparam>
+    public static void BindBitFlags<T>(this CheckBox checkBox, ReactiveProperty<T> property, T mask, out CompositeDisposable subs) where T : Enum
+    {
+        subs = new CompositeDisposable();
+
+        // reflect enum bits to checkbox pressed state
+        property.Subscribe(value => checkBox.ButtonPressed = value.HasFlag(mask)).AddTo(subs);
+
+        // update enum bits when checkbox toggled (use dynamic to keep it generic and avoid integer conversions)
+        checkBox
+            .OnToggledAsObservable()
+            .Subscribe(pressed =>
+            {
+                property.Value = pressed
+                    ? (T)((dynamic)property.Value | (dynamic)mask)
+                    : (T)((dynamic)property.Value & ~(dynamic)mask);
+            })
+            .AddTo(subs);
+    }
+
+    public static void BindBitFlags<T>(this CheckBox checkBox, ReactiveProperty<T> property, T mask) where T : Enum
+    {
+        BindBitFlags(checkBox, property, mask, out var sub);
+        sub.AddTo(checkBox);
     }
 }
