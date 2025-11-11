df1bb9645571d041ea7ddfe06cd40dd099ea3d43
ShenCiao <3294833183@qq.com>
2025-11-11 11:41:28 +0800

    Begin stroke deletion

---
+++ Ciallo/Command/DeleteStrokeCmd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Frent;
+
+namespace Ciallo.Command;
+
+public class DeleteStrokeCmd(Entity strokeE) : CommandBase
+{
+    public override void Do()
+    {
+        throw new NotImplementedException();
+    }
+    public override void Undo()
+    {
+        throw new NotImplementedException();
+    }
+}
