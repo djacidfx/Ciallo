cd9823673f0de10fa5ee729ea3fac08e5220ad11
ShenCiao <3294833183@qq.com>
2025-11-20 20:04:28 +0800

    Add system info collection button for debugging

--- Ciallo/NodeControl/MenuHelp.cs
+++ Ciallo/NodeControl/MenuHelp.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
 using Ciallo.Command;
 using Godot;
+using Environment = System.Environment;
 
 namespace Ciallo.NodeControl;
 
@@ -12,6 +16,7 @@ public partial class MenuHelp : PopupMenu
     {
         { "User manual", null },
         { "About Ciallo", null },
+        { "Copy system info", null },
     };
 
     public override void _Ready()
@@ -40,9 +45,41 @@ public partial class MenuHelp : PopupMenu
                 GetTree().GetNodesInGroup("Dialog").OfType<AcceptDialog>().First(n => n.Name == "AboutCiallo").Popup();
                 break;
             case 2:
+                DisplayServer.ClipboardSet(CollectSystemInfo());
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(id), $"Unhandled menu item index: {id}");
         }
+    }
+
+    public static string CollectSystemInfo()
+    {
+        var builder = new StringBuilder();
+        var device = RenderingServer.GetRenderingDevice();
+
+        builder.AppendLine($"OS: {Environment.OSVersion}");
+        var cpuNames = new List<string>();
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            using var searcher = new ManagementObjectSearcher("select Name from Win32_Processor");
+            foreach (var item in searcher.Get())
+            {
+                var name = item["Name"]?.ToString()?.Trim();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    cpuNames.Add(name);
+                }
+            }
+        }
+        if (cpuNames.Count == 0)
+        {
+            cpuNames.Add(RuntimeInformation.ProcessArchitecture.ToString());
+        }
+        builder.AppendLine($"CPU: {string.Join("|", cpuNames)}");
+        builder.AppendLine($"GPU: {device.GetDeviceName()}");
+        var driverInfo = OS.GetVideoAdapterDriverInfo();
+        builder.AppendLine($"Driver: {driverInfo[0]} {driverInfo[1]}");
+
+        return builder.ToString().TrimEnd();
     }
 }
