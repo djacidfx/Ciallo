d298f3104b36b7f272376fbcc6470181178cc7ee
ShenCiao <3294833183@qq.com>
2025-12-08 20:12:57 +0800

    Command builder methods

--- Ciallo/Command/CommandBuilder.cs
+++ Ciallo/Command/CommandBuilder.cs
@@ -1,15 +1,60 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Ciallo.Data;
 using Frent;
 
 namespace Ciallo.Command;
 
 public partial class CommandBuilder
 {
+    public readonly string Name = "Unnamed Action";
     public Entity TargetE;
-    public List<CommandBase> Commands = [];
+    public readonly List<CommandBase> Commands = [];
 
     public CommandBuilder(Entity targetE)
     {
         TargetE = targetE;
+    }
+
+    public CommandBuilder(string name, Entity targetE)
+    {
+        Name = name;
+        TargetE = targetE;
+    }
+
+    public CommandBuilder SetTarget(Entity e)
+    {
+        TargetE = e;
+        return this;
+    }
+
+    public void Commit(bool execute = true)
+    {
+        if (Commands.Count == 0) return;
+        var cm = TargetE.World.Document().Get<CommandManager>();
+
+        // Add Do/Undo Reference methods, order matters:
+        var objects = Commands.Select(c => new CommandWrapperObject(c)).ToArray();
+
+        cm.CreateAction(Name);
+        foreach (var obj in objects) cm.AddDo(obj);
+        foreach (var obj in objects.AsEnumerable().Reverse()) cm.AddUndo(obj);
+        cm.CommitAction(execute);
+    }
+
+    public void Do()
+    {
+        foreach (var cmd in Commands)
+        {
+            cmd.Do();
+        }
+    }
+
+    public void UnDo()
+    {
+        foreach (var cmd in Commands.AsEnumerable().Reverse())
+        {
+            cmd.Undo();
+        }
     }
 }
