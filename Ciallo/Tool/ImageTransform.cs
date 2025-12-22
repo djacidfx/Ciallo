e28ee46d0a3660c70ed6443cf5d6f728a105d5d2
ShenCiao <3294833183@qq.com>
2025-12-22 12:32:12 +0800

    Image transform tool

---
+++ Ciallo/Tool/ImageTransform.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Ciallo.Data;
+using Ciallo.Widget;
+using Frent;
+
+namespace Ciallo.Tool;
+
+[RegisterTool(ToolButton.Select)]
+public class ImageTransform : CommonToolBase
+{
+    public ImageTransform()
+    {
+        var hover = new ImageTransformHover();
+        HoverInteractor = hover;
+        LeftInteractor = new ImageTransformInteractor(hover);
+    }
+
+    public override void DrawProperty(PropertyContainer container)
+    {
+    }
+
+    public override bool CanHandleLayer(params Entity[] layerEs)
+    {
+        return layerEs.Length == 1 && layerEs.Single().Has<ImageLayerSetting>();
+    }
+}
