82300fbba9a1756e88e072ddf6a09354ec2f9214
ShenCiao <3294833183@qq.com>
2026-01-01 10:55:46 +0800

    Use abstract class for interactive session and rename Interactor base into Interactive session basde

---
+++ Ciallo/Tool/InteractiveSessionBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Ciallo.Geometry;
+using Frent;
+using Godot;
+
+namespace Ciallo.Tool;
+
+/// <summary>
+/// Interface for the classes that handle canvas interactions. Tools hold one or more of these interactive session.
+/// Implement users' tools logics and behaviors.
+/// </summary>
+/// <remarks>
+/// Key design idea:
+/// Separating interactive logic from how to trigger an interactive session。This allows us to support key remapping and more.
+/// E.g. Stroke drag interactor should not know it's started by dragging left mouse button, or pressing the 'G' key(like Blender)
+/// The tool script implement ITool is responsible for triggering the interactive session according to user input and tool state.
+/// </remarks>
+public abstract class InteractiveSessionBase
+{
+    public Entity Document { get; set; }
+    public Entity[] WorkingLayers;
+    public Entity WorkingLayer => WorkingLayers.Single();
+    public virtual void BeforeSrcEnd(InteractiveSessionBase session) { }
+    public void AfterSrcEnd(InteractiveSessionBase session) { }
+    public abstract void Start(CursorButtonData data);
+    public abstract void Interacting(CursorMotionData data);
+    public abstract void End(CursorButtonData data);
+    public void BeforeDstStart(InteractiveSessionBase session) { }
+    public void AfterDstStart(InteractiveSessionBase session) { }
+    public abstract void Cancel();
+    public abstract bool OnKey(InputEventKey key, CursorButtonData data);
+}
