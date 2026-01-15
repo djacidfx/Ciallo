e129f708f9ffca97181d231df11d95e5d8dbdd35
ShenCiao <3294833183@qq.com>
2026-01-15 23:19:48 +0800

    Configure output folders and lfs

--- GDExtension/src/register_types.cpp
+++ GDExtension/src/register_types.cpp
@@ -14,7 +14,7 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	GDREGISTER_CLASS(ExampleClass);
+	// GDREGISTER_CLASS(ExampleClass);
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
