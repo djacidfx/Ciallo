ad0c263cbc68ef7c33b33f14bada660d484c9da3
ShenCiao <3294833183@qq.com>
2026-01-20 00:52:56 +0800

    Refactor on creating filled polygon

--- Ciallo/Data/FilledPolygonSetting.cs
+++ Ciallo/Data/FilledPolygonSetting.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization;
+using Ciallo.Command;
 using Godot;
 using R3;
 
@@ -9,4 +10,11 @@ public class FilledPolygonSetting
 {
     // Same as Polygon2D fill color. If texture is set, it will be multiplied by this color.
     [DataMember] public ReactiveProperty<Color> Color = new(Colors.White);
+
+    public CompositeDisposable RegisterProperties(CommandManager manager)
+    {
+        CompositeDisposable subs = new();
+        manager.RegisterProperty(Color).AddTo(subs);
+        return subs;
+    }
 }
