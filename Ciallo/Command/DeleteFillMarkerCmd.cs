1264b417105058a7ca2cfb57b9faccde8632b718
ShenCiao <3294833183@qq.com>
2026-01-20 00:49:25 +0800

    Vector fill settings and delete commands

---
+++ Ciallo/Command/DeleteFillMarkerCmd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Ciallo.Data;
+using Ciallo.GuiControl;
+using Ciallo.Rendering;
+using Frent;
+using Godot;
+
+namespace Ciallo.Command;
+
+public class DeleteFillMarkerCmd : CommandBase
+{
+    public override IEnumerable<Entity> UndoRefEntities => ToEnumerable(TargetE);
+    
+    protected override void Do(Entity targetE)
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    protected override void Undo(Entity targetE)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Ciallo/Data/VectorFillLayerSetting.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using Frent;
+using R3;
+using ObservableCollections;
+
+namespace Ciallo.Data;
+
+[DataContract, ToSerialize]
+public class VectorFillLayerSetting
+{
+    [DataMember] public ObservableList<Entity> ReferenceLayerEs = [];
+}
