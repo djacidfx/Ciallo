a6fa154460ae6138270bb252bbe4ef5ebae13fb5
ShenCiao <3294833183@qq.com>
2026-02-03 23:48:31 +0800

    Add polyline query binding and memory tracker

--- Ciallo/Geometry/Arrangement2D.cs
+++ Ciallo/Geometry/Arrangement2D.cs
@@ -1,5 +1,8 @@
-﻿using Godot;
-using Godot.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Array = Godot.Collections.Array;
 
 namespace Ciallo.Geometry;
 
@@ -11,32 +14,48 @@ namespace Ciallo.Geometry;
 public class Arrangement2D
 {
     private readonly GodotObject _obj = (GodotObject)ClassDB.Instantiate("Arrangement2D");
+    public static readonly int MemoryPerPoint = 200; // byte in very rough estimation
+    private readonly Dictionary<Rid, int> _polylineLengthTracker = new();
 
     public Rid CreatePolyline()
     {
-        return (Rid)_obj.Call("create_polyline");
+        var id = (Rid)_obj.Call("create_polyline");
+        _polylineLengthTracker.Add(id, 0);
+        return id;
     }
-    
+
     /// <returns>Array of face Rids that are returned in previous queries and invalid since polyline change</returns>
     public Array SetPolyline(Rid id, Vector2[] data)
     {
+        GC.RemoveMemoryPressure(MemoryPerPoint * _polylineLengthTracker[id]);
+        _polylineLengthTracker[id] = data.Length;
+        GC.AddMemoryPressure(MemoryPerPoint * data.Length);
         return (Array)_obj.Call("set_polyline", id, data);
     }
 
     /// <returns>Array of face Rids that are returned in previous queries and invalid since removing polyline</returns>
     public Array RemovePolyline(Rid id)
     {
+        GC.RemoveMemoryPressure(MemoryPerPoint * _polylineLengthTracker[id]);
+        _polylineLengthTracker.Remove(id);
         return (Array)_obj.Call("remove_polyline", id);
     }
 
+    /// <returns>A face rid</returns>
     public Rid Query(Vector2 point)
     {
         return (Rid)_obj.Call("query", point);
     }
 
+    /// <returns>An array of face rids</returns>
     public Array BatchQuery(Vector2[] points)
     {
         return (Array)_obj.Call("batch_query", points);
+    }
+
+    public Array PolylineQuery(Vector2[] polyline)
+    {
+        return (Array)_obj.Call("polyline_query", polyline);
     }
 
     public Vector2[] FaceGetPolygon(Rid id)
@@ -51,6 +70,9 @@ public class Arrangement2D
 
     ~Arrangement2D()
     {
-        _obj.Free();
+        GC.RemoveMemoryPressure(_polylineLengthTracker.Values.Sum() * MemoryPerPoint);
+        _polylineLengthTracker.Clear();
+        // Finalizer run in its own thread. Call_deferred makes it run in the main thread
+        _obj.CallDeferred("free");
     }
 }
