64d34077ed958e66ca1b30d6e892403ae039f61d
ShenCiao <3294833183@qq.com>
2026-02-09 10:29:20 +0800

    Fix wrapper deletion order

--- Ciallo/Command/CommandManager.cs
+++ Ciallo/Command/CommandManager.cs
@@ -15,21 +15,21 @@ public partial class CommandManager : UndoRedo
 
     public CommandManager()
     {
-        SetMaxSteps(30);
+        SetMaxSteps(3);
     }
 
     public void AddDo(CommandWrapperObject cmdWrapper)
     {
         AddDoMethod(new(cmdWrapper, CommandWrapperObject.MethodName.Do));
+        AddDoReference(cmdWrapper); // order matters, first add first delete
         AddDoReference(cmdWrapper.DoDeleter);
-        AddDoReference(cmdWrapper);
     }
 
     public void AddUndo(CommandWrapperObject cmdWrapper)
     {
         AddUndoMethod(new(cmdWrapper, CommandWrapperObject.MethodName.Undo));
+        AddUndoReference(cmdWrapper); // order matters
         AddUndoReference(cmdWrapper.UndoDeleter);
-        AddUndoReference(cmdWrapper);
     }
 
     public static bool SkipPropertyCommit = false; // not thread safe
