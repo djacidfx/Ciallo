a7d7cf45343e48990aab12585880dcfb9ad3d237
ShenCiao <3294833183@qq.com>
2026-02-13 11:27:22 +0800

    New image layer refactor

--- Ciallo/Command/NewImageLayerCmd.cs
+++ Ciallo/Command/NewImageLayerCmd.cs
@@ -14,7 +14,6 @@ public class NewImageLayerCmd : CommandBase
 {
     private readonly ImageLayerSetting _setting;
     private CommonLayerSetting _commonSetting;
-    private CompositeDisposable _subs;
 
     public override IEnumerable<Entity> DoRefEntities => ToEnumerable(TargetE);
 
@@ -35,56 +34,55 @@ public class NewImageLayerCmd : CommandBase
 
     public override void BeforeFirstDo(Entity layerE)
     {
+        // Data
         layerE.Add(new LayerTreeNode());
-
         _commonSetting ??= new CommonLayerSetting { Name = { Value = "Image".Tr() } };
         layerE.Add(_commonSetting);
         _commonSetting.RegisterProperties(CommandManager).AddTo(layerE);
         layerE.Add(_setting);
+        CommandManager.RegisterProperty(_setting.ImageTransform).AddTo(layerE);
 
+        // View
         var sprite = new Sprite2D
         {
             Texture = _setting.Texture,
         };
         layerE.AddNode(sprite);
 
-        var layerOverlay = new TransformOverlayBox(_setting.ImageSize) { Visible = false };
-        layerE.AddNode(layerOverlay);
-    }
-
-    public override void Do(Entity layerE)
-    {
-        _subs = new();
-        _subs.AddTo(layerE);
-
-        layerE.Tag<ToSerializeTag>();
-        Document.Get<LayerTreeNode>().AddChild(layerE);
-        CommandManager.RegisterProperty(_setting.ImageTransform).AddTo(_subs);
-
-        // View
-        var worldView = Document.Get<WorldView>();
-        var sprite = layerE.Get<Sprite2D>();
-        worldView.AddChild(sprite);
-        _setting.ImageTransform.Subscribe(sprite.SetTransform).AddTo(_subs);
-        sprite.SetOwner(worldView);
-
-        _commonSetting.IsVisible.Subscribe(sprite.SetVisible).AddTo(_subs);
+        _commonSetting.IsVisible.Subscribe(sprite.SetVisible).AddTo(layerE);
         _commonSetting.Opacity.Subscribe(v =>
         {
             var color = sprite.SelfModulate;
             color.A = v;
             sprite.SelfModulate = color;
-        }).AddTo(_subs);
+        }).AddTo(layerE);
+        _setting.ImageTransform.Subscribe(sprite.SetTransform).AddTo(layerE);
+
+        // Overlay
+        var layerOverlay = new TransformOverlayBox(_setting.ImageSize) { Visible = false };
+        layerE.AddNode(layerOverlay);
+        _setting.ImageTransform.Subscribe(t =>
+        {
+            layerOverlay.LocalTransform = t;
+            layerOverlay.UpdateGeometry();
+        }).AddTo(layerE);
+    }
+
+    public override void Do(Entity layerE)
+    {
+        layerE.Tag<ToSerializeTag>();
+        Document.Get<LayerTreeNode>().AddChild(layerE);
+
+        // View
+        var worldView = Document.Get<WorldView>();
+        var sprite = layerE.Get<Sprite2D>();
+        worldView.AddChild(sprite);
+        sprite.SetOwner(worldView);
 
         // Overlay
         var worldOverlay = Document.Get<WorldOverlay>();
         var layerOverlay = layerE.Get<TransformOverlayBox>();
         worldOverlay.AddChild(layerOverlay);
-        _setting.ImageTransform.Subscribe(t =>
-        {
-            layerOverlay.LocalTransform = t;
-            layerOverlay.UpdateGeometry();
-        }).AddTo(_subs);
 
         // Panel
         var layerContainer = Document.Get<LayerContainer>();
@@ -106,7 +104,5 @@ public class NewImageLayerCmd : CommandBase
         // Data
         Document.Get<LayerTreeNode>().RemoveChild(layerE);
         layerE.Detach<ToSerializeTag>();
-
-        _subs.Dispose();
     }
 }
