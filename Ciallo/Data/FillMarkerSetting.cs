052bfd61f4265683daec9a0818b8045325ce9eaf
ShenCiao <3294833183@qq.com>
2026-03-05 21:11:08 +0800

    IEquatable<FillMarkerSetting>

--- Ciallo/Data/FillMarkerSetting.cs
+++ Ciallo/Data/FillMarkerSetting.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 using Frent;
 using Godot;
 using R3;
@@ -6,7 +7,7 @@ using R3;
 namespace Ciallo.Data;
 
 [DataContract, ToSerialize]
-public class FillMarkerSetting
+public class FillMarkerSetting : IEquatable<FillMarkerSetting>
 {
     // Marker use stroke/polyline has single point
     [DataMember(Order = 0)] public ReactiveProperty<Entity> StrokeBrushE = new(default);
@@ -22,5 +23,26 @@ public class FillMarkerSetting
             StrokeOverrideColor = { Value = StrokeOverrideColor.Value },
             FillColor = { Value = FillColor.Value },
         };
+    }
+
+    public bool Equals(FillMarkerSetting other)
+    {
+        if (ReferenceEquals(other, null)) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        return Equals(StrokeBrushE.Value, other.StrokeBrushE.Value)
+               && Nullable.Equals(StrokeOverrideColor.Value, other.StrokeOverrideColor.Value)
+               && FillColor.Value.Equals(other.FillColor.Value);
+    }
+
+    public override bool Equals(object? obj) => obj is FillMarkerSetting other && Equals(other);
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(
+            StrokeBrushE.Value,
+            StrokeOverrideColor.Value,
+            FillColor.Value
+        );
     }
 }
