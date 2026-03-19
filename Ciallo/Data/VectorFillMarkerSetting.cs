5a9ef461fb4cc7825722d0de1b4835eb6f2dc17f
ShenCiao <3294833183@qq.com>
2026-03-19 21:18:51 +0800

    Clean unused field

--- Ciallo/Data/VectorFillMarkerSetting.cs
+++ Ciallo/Data/VectorFillMarkerSetting.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Runtime.Serialization;
 using Frent;
-using Godot;
 using R3;
 
 namespace Ciallo.Data;
@@ -11,14 +10,12 @@ public class VectorFillMarkerSetting : IEquatable<VectorFillMarkerSetting>
 {
     // Marker use stroke/polyline has single point
     [DataMember] public ReactiveProperty<Entity> BrushE = new(default);
-    [DataMember] public ReactiveProperty<Color?> MarkerOverridingColor = new(null);
 
     public VectorFillMarkerSetting Clone()
     {
         return new VectorFillMarkerSetting
         {
             BrushE = { Value = BrushE.Value },
-            MarkerOverridingColor = { Value = MarkerOverridingColor.Value },
         };
     }
 
@@ -27,8 +24,7 @@ public class VectorFillMarkerSetting : IEquatable<VectorFillMarkerSetting>
         if (ReferenceEquals(other, null)) return false;
         if (ReferenceEquals(this, other)) return true;
 
-        return Equals(BrushE.Value, other.BrushE.Value)
-               && Nullable.Equals(MarkerOverridingColor.Value, other.MarkerOverridingColor.Value);
+        return Equals(BrushE.Value, other.BrushE.Value);
     }
 
     public override bool Equals(object obj) => obj is VectorFillMarkerSetting other && Equals(other);
@@ -36,8 +32,7 @@ public class VectorFillMarkerSetting : IEquatable<VectorFillMarkerSetting>
     public override int GetHashCode()
     {
         return HashCode.Combine(
-            BrushE.Value,
-            MarkerOverridingColor.Value
+            BrushE.Value
         );
     }
 }
