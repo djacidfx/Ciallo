681a80d3dccbc5fc9a675d58d5b2cfb6c7def8f7
ShenCiao <3294833183@qq.com>
2026-03-23 12:07:22 +0800

    Add Flatten mechanics

--- Ciallo/Misc/ReactivePropertyExtension.cs
+++ Ciallo/Misc/ReactivePropertyExtension.cs
@@ -7,6 +7,11 @@ namespace Ciallo;
 
 public static class ReactivePropertyExtension
 {
+    /// <summary>
+    /// Create a ReactiveProperty two-way binded to the original one through the given selectors.
+    /// Commonly used for unit conversion.
+    /// </summary>
+    /// <returns></returns>
     public static ReactiveProperty<TTarget> Project<TSource, TTarget>(
         this ReactiveProperty<TSource> source,
         Func<TSource, TTarget> readSelector,
@@ -31,6 +36,35 @@ public static class ReactivePropertyExtension
         view.Subscribe(v => source.Value = writeSelector(v)).AddTo(subs);
         source.Subscribe(v => view.Value = readSelector(v)).AddTo(subs);
         return view;
+    }
+
+    /// <summary>
+    /// Project inner ReactiveProperty outside, similiar to Switch() operator
+    /// </summary>
+    public static ReactiveProperty<T> Flatten<T>(this Observable<ReactiveProperty<T>> outer, out CompositeDisposable subs)
+    {
+        var result = new ReactiveProperty<T>();
+        subs = new();
+
+        IDisposable innerSub = null;
+        ReactiveProperty<T> currentInner = null;
+
+        outer.Subscribe(inner =>
+        {
+            innerSub?.Dispose();
+            currentInner = inner;
+            result.Value = inner.Value;
+            innerSub = inner.Subscribe(v => result.Value = v);
+        }).AddTo(subs);
+
+        result.Subscribe(v =>
+        {
+            currentInner?.Value = v;
+        }).AddTo(subs);
+
+        Disposable.Create(() => innerSub?.Dispose()).AddTo(subs);
+
+        return result;
     }
 }
 
