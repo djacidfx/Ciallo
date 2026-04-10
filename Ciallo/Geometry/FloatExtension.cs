63fbc7c9faa0e4cf95223b26cc166c7d8158eba4
ShenCiao <3294833183@qq.com>
2026-04-10 09:59:59 +0800

    Float Modf

--- Ciallo/Geometry/FloatExtension.cs
+++ Ciallo/Geometry/FloatExtension.cs
@@ -1,15 +1,17 @@
-﻿using Godot;
+﻿using System;
+using System.Diagnostics.Contracts;
+using Godot;
 
 namespace Ciallo.Geometry;
 
-public static class FloatRemap
+public static class FloatExtension
 {
-    public static float Remap(this float value, float fromMin, float fromMax, float toMin, float toMax)
+    [Pure] public static float Remap(this float value, float fromMin, float fromMax, float toMin, float toMax)
     {
         return Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
     }
 
-    public static float SmoothRemap(this float value, float fromMin, float fromMax, float toMin, float toMax)
+    [Pure] public static float SmoothRemap(this float value, float fromMin, float fromMax, float toMin, float toMax)
     {
         float t = Mathf.InverseLerp(fromMin, fromMax, value);
         t = t * t * (3f - 2f * t); // Smoothstep
@@ -19,12 +21,19 @@ public static class FloatRemap
     // Map input to [toMin, toMax], the sensitive range has wider change rate.
     // Different from SmoothRemap, this can map toMin/toMax even if value is out of sensitive range.
     // Take 95% value domine [-3.6635, 3.6635] as sensitive value range.
-    public static float SigmoidRemap(this float value, float sensitiveMin, float sensitiveMax, float toMin, float toMax)
+    [Pure] public static float SigmoidRemap(this float value, float sensitiveMin, float sensitiveMax, float toMin, float toMax)
     {
         float t = Mathf.InverseLerp(sensitiveMin, sensitiveMax, value);
         t = t * 2 - 1; // Map to [-1, 1]
         t *= 3.6635f;
         t = 1f / (1f + Mathf.Exp(-t)); // Sigmoid
         return Mathf.Lerp(toMin, toMax, t);
+    }
+    
+    [Pure] public static (int i, float f) Modf(this float v)
+    {
+        int i = (int)Math.Floor(v);
+        float frac = v - i;
+        return (i, frac);
     }
 }
