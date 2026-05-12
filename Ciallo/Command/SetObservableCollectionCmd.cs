88c727830265d1177e81d7df03dd36b784c2c9f9
ShenCiao <3294833183@qq.com>
2026-05-12 23:11:40 +0800

    Refactor SetObservableCollection

--- Ciallo/Command/SetObservableCollectionCmd.cs
+++ Ciallo/Command/SetObservableCollectionCmd.cs
@@ -8,17 +8,16 @@ using R3;
 
 namespace Ciallo.Command;
 
-public class SetObservableCollection<TValue> : CommandBase
+public abstract class SetObservableCollectionBase<TCollection> : CommandBase
 {
-    public Func<Entity, IObservableCollection<TValue>> GetCollection { get; }
-    public Action<IObservableCollection<TValue>> Action { get; }
-    public readonly List<CollectionChangedEvent<TValue>> CollectionHistory = [];
+    public Func<Entity, TCollection> GetCollection { get; }
+    public Action<TCollection> Action { get; }
 
-    public IObservableCollection<TValue> Collection { get; private set; }
+    public TCollection Collection;
 
-    public SetObservableCollection(
-        Func<Entity, IObservableCollection<TValue>> getCollection,
-        Action<IObservableCollection<TValue>> action)
+    protected SetObservableCollectionBase(
+        Func<Entity, TCollection> getCollection,
+        Action<TCollection> action)
     {
         GetCollection = getCollection;
         Action = action;
@@ -28,6 +27,16 @@ public class SetObservableCollection<TValue> : CommandBase
     {
         Collection = GetCollection(targetE);
     }
+}
+
+public class SetObservableList<T> : SetObservableCollectionBase<ObservableList<T>>
+{
+    public readonly List<CollectionChangedEvent<T>> CollectionHistory = [];
+
+    public SetObservableList(
+        Func<Entity, ObservableList<T>> getCollection,
+        Action<ObservableList<T>> action
+    ) : base(getCollection, action) { }
 
     public override void Do(Entity targetE)
     {
@@ -37,56 +46,101 @@ public class SetObservableCollection<TValue> : CommandBase
 
     public override void Undo(Entity targetE)
     {
-        // Undo according to history
         var reversedHistory = CollectionHistory.AsEnumerable().Reverse();
-        switch (Collection)
+        var list = Collection;
+        foreach (var et in reversedHistory)
         {
-            case ObservableList<TValue> list:
-                foreach (var et in reversedHistory)
-                {
-                    switch (et.Action)
-                    {
-                        case NotifyCollectionChangedAction.Add:
-                            list.RemoveAt(et.NewStartingIndex);
-                            break;
-                        case NotifyCollectionChangedAction.Remove:
-                            list.Insert(et.OldStartingIndex, et.OldItem);
-                            break;
-                        case NotifyCollectionChangedAction.Replace:
-                            list[et.NewStartingIndex] = et.OldItem;
-                            break;
-                        case NotifyCollectionChangedAction.Move:
-                            list.Move(et.NewStartingIndex, et.OldStartingIndex);
-                            break;
-                        default:
-                            throw new NotSupportedException($"Undo is not supported for action {et.Action}");
-                    }
-                }
-                break;
-            case ObservableHashSet<TValue> set:
-                foreach (var et in reversedHistory)
-                {
-                    switch (et.Action)
-                    {
-                        case NotifyCollectionChangedAction.Add:
-                            set.Remove(et.NewItem);
-                            break;
-                        case NotifyCollectionChangedAction.Remove:
-                            set.Add(et.OldItem);
-                            break;
-                        default:
-                            throw new NotSupportedException($"Undo is not supported for action {et.Action}");
-                    }
-                }
-                break;
-            default:
-                throw new NotSupportedException($"Collection type {Collection.GetType()} is not supported yet");
+            switch (et.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    list.RemoveAt(et.NewStartingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    list.Insert(et.OldStartingIndex, et.OldItem);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    list[et.NewStartingIndex] = et.OldItem;
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    list.Move(et.NewStartingIndex, et.OldStartingIndex);
+                    break;
+                default:
+                    throw new NotSupportedException($"Undo is not supported for action {et.Action}");
+            }
         }
+    }
+}
 
-        // May need reflection (or ICollection<TPair>) to deal with ObservableDictionary, which is not supported yet.
+public class SetObservableHashSet<T> : SetObservableCollectionBase<ObservableHashSet<T>>
+{
+    public readonly List<CollectionChangedEvent<T>> CollectionHistory = [];
 
-        // Clear history
-        CollectionHistory.Clear();
+    public SetObservableHashSet(
+        Func<Entity, ObservableHashSet<T>> getCollection,
+        Action<ObservableHashSet<T>> action
+    ) : base(getCollection, action) { }
+
+    public override void Do(Entity targetE)
+    {
+        using var _ = Collection.ObserveChanged().Subscribe(CollectionHistory.Add);
+        Action(Collection);
+    }
+
+    public override void Undo(Entity targetE)
+    {
+        var reversedHistory = CollectionHistory.AsEnumerable().Reverse();
+        foreach (var et in reversedHistory)
+        {
+            switch (et.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Collection.Remove(et.NewItem);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Collection.Add(et.OldItem);
+                    break;
+                default:
+                    throw new NotSupportedException($"Undo is not supported for action {et.Action}");
+            }
+        }
+    }
+}
+
+public class SetObservableDictionary<TKey, TValue> : SetObservableCollectionBase<ObservableDictionary<TKey, TValue>>
+{
+    public readonly List<CollectionChangedEvent<KeyValuePair<TKey, TValue>>> CollectionHistory = [];
+
+    public SetObservableDictionary(
+        Func<Entity, ObservableDictionary<TKey, TValue>> getCollection,
+        Action<ObservableDictionary<TKey, TValue>> action
+    ) : base(getCollection, action) { }
+
+    public override void Do(Entity targetE)
+    {
+        using var _ = Collection.ObserveChanged().Subscribe(CollectionHistory.Add);
+        Action(Collection);
+    }
+
+    public override void Undo(Entity targetE)
+    {
+        var reversedHistory = CollectionHistory.AsEnumerable().Reverse();
+        foreach (var et in reversedHistory)
+        {
+            switch (et.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Collection.Remove(et.NewItem.Key);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Collection.Add(et.OldItem.Key, et.OldItem.Value);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    Collection[et.NewItem.Key] = et.OldItem.Value;
+                    break;
+                default:
+                    throw new NotSupportedException($"Undo is not supported for action {et.Action}");
+            }
+        }
     }
 }
 
@@ -96,21 +150,26 @@ public partial class CommandBuilder
         Func<Entity, ObservableList<T>> getCollection,
         Action<ObservableList<T>> action)
     {
-        var cmd = new SetObservableCollection<T>(getCollection, AdaptedAction) { TargetE = TargetE };
+        var cmd = new SetObservableList<T>(getCollection, action) { TargetE = TargetE };
         Commands.Add(cmd);
         return this;
-
-        void AdaptedAction(IObservableCollection<T> collection) => action((ObservableList<T>)collection);
     }
 
     public CommandBuilder SetObservableCollection<T>(
         Func<Entity, ObservableHashSet<T>> getCollection,
         Action<ObservableHashSet<T>> action)
     {
-        var cmd = new SetObservableCollection<T>(getCollection, AdaptedAction) { TargetE = TargetE };
+        var cmd = new SetObservableHashSet<T>(getCollection, action) { TargetE = TargetE };
         Commands.Add(cmd);
         return this;
+    }
 
-        void AdaptedAction(IObservableCollection<T> collection) => action((ObservableHashSet<T>)collection);
+    public CommandBuilder SetObservableCollection<TKey, TValue>(
+        Func<Entity, ObservableDictionary<TKey, TValue>> getCollection,
+        Action<ObservableDictionary<TKey, TValue>> action)
+    {
+        var cmd = new SetObservableDictionary<TKey, TValue>(getCollection, action) { TargetE = TargetE };
+        Commands.Add(cmd);
+        return this;
     }
 }
