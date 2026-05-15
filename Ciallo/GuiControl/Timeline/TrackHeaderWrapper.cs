13e2bc838f2bef05ee1c1846eb09795b7ac3fed9
ShenCiao <3294833183@qq.com>
2026-05-15 17:26:28 +0800

    Remove unused header node script

--- Ciallo/GuiControl/Timeline/TrackHeaderWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ciallo.GuiControl;
-
-/// <summary>
-/// Wraps a timeline track-header's block.
-/// Structurally identical to <see cref="LayerWrapper"/> but is a distinct type so that
-/// each layer entity can hold BOTH a <see cref="LayerWrapper"/> (for the Layer panel)
-/// and a <see cref="TrackHeaderWrapper"/> (for the Timeline header) as separate Frent components.
-/// </summary>
-public partial class TrackHeaderWrapper : LayerWrapper;
