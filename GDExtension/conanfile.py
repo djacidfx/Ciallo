0b42f7986f39e5462e6f0cc4e83091dfbadbdb09
ShenCiao <3294833183@qq.com>
2026-05-27 21:37:41 +0800

    Remove GDExtension and its submodule

--- GDExtension/conanfile.py
+++
@@ -1,8 +0,0 @@
-from conan import ConanFile
-from conan.tools.scons import SConsDeps
-
-class CGALConsumerConan(ConanFile):
-    generators = "SConsDeps"
-
-    def requirements(self):
-        self.requires("cgal/6.1")
