1dd9db557dbbd9066207f0f44b39490d2b5eb475
ShenCiao <3294833183@qq.com>
2026-06-02 13:06:54 +0800

    Add overloads for VisibleIf<bool>

--- Ciallo/Misc/GodotControlExtension.cs
+++ Ciallo/Misc/GodotControlExtension.cs
@@ -8,6 +8,20 @@ namespace Ciallo;
 
 public static class GodotControlExtension
 {
+    public static TControl VisibleIf<TControl>(this TControl control, Observable<bool> visible, CompositeDisposable disposables) where TControl : Control
+    {
+        var sub = visible.Subscribe(value => control.Visible = value);
+        disposables.Add(sub);
+        return control;
+    }
+
+    public static TControl VisibleIf<TControl>(this TControl control, Observable<bool> visible) where TControl : Control
+    {
+        var sub = visible.Subscribe(value => control.Visible = value);
+        sub.AddTo(control);
+        return control;
+    }
+
     public static TControl VisibleIf<TControl, T>(this TControl control, Observable<T> property, Predicate<T> predicate, CompositeDisposable disposables) where TControl : Control
     {
         var sub = property.Subscribe(value => control.Visible = predicate(value));
