6eb90f61734505f127d3c0213f2b8147a5f396c8
ShenCiao <3294833183@qq.com>
2026-06-08 00:23:14 +0800

    Refactor Arrangement2D class by removing unused methods and fields

--- Ciallo/Geometry/Arrangement2D.cs
+++ Ciallo/Geometry/Arrangement2D.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Runtime.InteropServices;
 using Godot;
 using Godot.Collections;
@@ -12,12 +11,6 @@ namespace Ciallo.Geometry;
 // Need manually call Dispose(), don't call Free() directly.
 public partial class Arrangement : Godot.Arrangement2D
 {
-    public static readonly int MemoryPerPoint = 200; // byte in very rough estimation
-    private readonly System.Collections.Generic.Dictionary<Rid, int> _polylineLengthTracker = new();
-    private bool _nativeFreeQueued;
-
-    public readonly Subject<Unit> StructureChanged = new();
-
     public void SetPolyline(Rid id, ImmutableArray<Vector2> data)
     {
         SetPolyline(id, ImmutableCollectionsMarshal.AsArray(data));
@@ -28,63 +21,9 @@ public partial class Arrangement : Godot.Arrangement2D
         return PolylineQueryEdges(ImmutableCollectionsMarshal.AsArray(polyline));
     }
 
-    # region GDExtension bindings
-
-    public new Rid CreatePolyline()
-    {
-        var id = base.CreatePolyline();
-        _polylineLengthTracker.Add(id, 0);
-        return id;
-    }
-
-    public new void SetPolyline(Rid id, Vector2[] data)
-    {
-        SetPolyline(id, data.AsSpan());
-    }
-
-    /// <returns>Array of face Rids that are returned in previous queries and invalid since polyline change</returns>
-    public new void SetPolyline(Rid id, ReadOnlySpan<Vector2> data)
-    {
-        if (_polylineLengthTracker[id] > 0)
-            GC.RemoveMemoryPressure(MemoryPerPoint * _polylineLengthTracker[id]);
-        _polylineLengthTracker[id] = data.Length;
-        if (data.Length > 0)
-            GC.AddMemoryPressure(MemoryPerPoint * data.Length);
-        base.SetPolyline(id, data);
-        StructureChanged.OnNext(Unit.Default);
-    }
-
-    /// <returns>Array of face Rids that are returned in previous queries and invalid since removing polyline</returns>
-    public new void RemovePolyline(Rid id)
-    {
-        bool toNotify = _polylineLengthTracker[id] != 0;
-        if (_polylineLengthTracker[id] > 0)
-            GC.RemoveMemoryPressure(MemoryPerPoint * _polylineLengthTracker[id]);
-        _polylineLengthTracker.Remove(id);
-        base.RemovePolyline(id);
-        if (toNotify)
-            StructureChanged.OnNext(Unit.Default);
-    }
-
     protected override void Dispose(bool disposing)
     {
-        ReleaseMemoryPressure();
-        if (!_nativeFreeQueued && NativeInstance != IntPtr.Zero)
-        {
-            _nativeFreeQueued = true;
-            // Native Arrangement2D is an Object, not RefCounted; dispose only disconnects the managed binding.
-            CallDeferred(GodotObject.MethodName.Free);
-        }
-        StructureChanged.Dispose();
+        CallDeferred(GodotObject.MethodName.Free);
         base.Dispose(disposing);
     }
-
-    private void ReleaseMemoryPressure()
-    {
-        int sum = _polylineLengthTracker.Values.Sum();
-        if (sum > 0) GC.RemoveMemoryPressure(sum * MemoryPerPoint);
-        _polylineLengthTracker.Clear();
-    }
-
-    # endregion
 }
